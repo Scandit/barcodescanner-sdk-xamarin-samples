042413b4ac4e4269f9d6971292bfabe6eac4c83d
Franciszek Gonciarz <franciszek@scandit.com>
2018-07-03 17:06:34 +0200

    Add Split View Sample

---
+++ Native/iOSSplitViewSample/iOSAimAndScanSample/SplitViewResultTableSource.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Foundation;
+using UIKit;
+
+namespace iOSSplitViewSample
+{
+    public class SplitViewResultTableSource: UITableViewSource
+    {
+
+        List<SplitViewResult> tableItems;
+        string cellIdentifier = "SplitViewResultTableViewCell";
+
+        public SplitViewResultTableSource(List<SplitViewResult> items)
+        {
+            tableItems = items;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return tableItems.Count;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            SplitViewResultTableViewCell cell = tableView.DequeueReusableCell(cellIdentifier) as SplitViewResultTableViewCell;
+            cell.setBarcode(tableItems[indexPath.Row].barcode);
+            cell.setSymbology(tableItems[indexPath.Row].symbology);
+            return cell;
+        }
+
+        public override nint NumberOfSections(UITableView tableView)
+        {
+            return 1;
+        }
+
+    }
+}
---
+++ Native/iOSSplitViewSample/iOSAimAndScanSample/SplitViewResultTableViewController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UIKit;
+using Foundation;
+
+namespace iOSSplitViewSample
+{
+    public partial class SplitViewResultTableViewController : UIViewController
+    {
+
+        public List<SplitViewResult> splitViewResults = new List<SplitViewResult>();
+
+        public SplitViewResultTableViewController() : base("SplitViewResultTableViewController", null)
+        {
+        }
+
+        public SplitViewResultTableViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            splitViewResultsTableView.Source = new SplitViewResultTableSource(splitViewResults);
+            splitViewResultsTableView.ReloadData();
+        }
+
+        public void AddResult(SplitViewResult result)
+        {
+            splitViewResults.Insert(0, result);
+            splitViewResultsTableView.Source = new SplitViewResultTableSource(splitViewResults);
+            splitViewResultsTableView.BeginUpdates();
+            splitViewResultsTableView.InsertRows(new[] { NSIndexPath.FromItemSection(0, 0) }, UITableViewRowAnimation.Top);
+            splitViewResultsTableView.EndUpdates();
+        }
+
+        public void ClearResults()
+        {
+            splitViewResults = new List<SplitViewResult>();
+            splitViewResultsTableView.Source = new SplitViewResultTableSource(splitViewResults);
+            splitViewResultsTableView.ReloadData();
+        }
+    }
+}
+
