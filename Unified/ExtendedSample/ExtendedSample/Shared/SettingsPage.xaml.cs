7bcf735473fe9e71341883db754a4140491258d5
Tomasz Jurkowski <tomasz@scandit.com>
2018-08-24 13:32:23 +0200

    Tabs ---> Spaces refactoring.

--- Unified/ExtendedSample/ExtendedSample/Shared/SettingsPage.xaml.cs
+++ Unified/ExtendedSample/ExtendedSample/Shared/SettingsPage.xaml.cs
@@ -22,73 +22,73 @@ using Xamarin.Forms;
 namespace ExtendedSample
 {
     public partial class SettingsPage : ContentPage
-	{
-		private IBarcodePicker _picker;
-		private ScanSettings _scanSettings;
+    {
+        private IBarcodePicker _picker;
+        private ScanSettings _scanSettings;
 
-		public SettingsPage()
-		{
-			InitializeComponent();
+        public SettingsPage()
+        {
+            InitializeComponent();
 
-			_picker = ScanditService.BarcodePicker;
-			_scanSettings = _picker.GetDefaultScanSettings();
+            _picker = ScanditService.BarcodePicker;
+            _scanSettings = _picker.GetDefaultScanSettings();
 
-			initializeGuiElements();
+            initializeGuiElements();
 
-			// restore the currently used settings to those found in
-			// the permanent storage.
-			updateScanSettings();
-			updateScanOverlay();
-		}
+            // restore the currently used settings to those found in
+            // the permanent storage.
+            updateScanSettings();
+            updateScanOverlay();
+        }
 
-		private void initializeGuiElements()
-		{
-			// Adds all the symbology Switches (including inverse Switches)
-			// to SymbologySection
-			bool isMsiPlesseyThrough = false;
-			foreach (string setting in Convert.settingToSymbologies.Keys)
-			{
-				addSymbologySwitches(setting, isMsiPlesseyThrough ? 0 : 1);
-				if (Convert.settingToSymbologies[setting] != null
+        private void initializeGuiElements()
+        {
+            // Adds all the symbology Switches (including inverse Switches)
+            // to SymbologySection
+            bool isMsiPlesseyThrough = false;
+            foreach (string setting in Convert.settingToSymbologies.Keys)
+            {
+                addSymbologySwitches(setting, isMsiPlesseyThrough ? 0 : 1);
+                if (Convert.settingToSymbologies[setting] != null
                     && Convert.settingToSymbologies[setting][0] == Symbology.MsiPlessey)
-				{
-					isMsiPlesseyThrough = true;
-				}
-			}
-			initializeMsiPlesseyChecksumPicker();
+                {
+                    isMsiPlesseyThrough = true;
+                }
+            }
+            initializeMsiPlesseyChecksumPicker();
 
-			// Initialize Feedback
-			initializeSwitch(BeepCell, Settings.BeepString);
-			initializeSwitch(VibrateCell, Settings.VibrateString);
+            // Initialize Feedback
+            initializeSwitch(BeepCell, Settings.BeepString);
+            initializeSwitch(VibrateCell, Settings.VibrateString);
 
-			// Initialize Torch
-			initializeSwitch(TorchButtonCell, Settings.TorchButtonString);
+            // Initialize Torch
+            initializeSwitch(TorchButtonCell, Settings.TorchButtonString);
 
-			// Initialize Camera (gui-button-settings)
-			initializeCameraPicker();
+            // Initialize Camera (gui-button-settings)
+            initializeCameraPicker();
 
-			// Initialize Gui Style Picker
-			initializeGuiStylePicker();
+            // Initialize Gui Style Picker
+            initializeGuiStylePicker();
 
-			// Initialize Hotspot
-			initializeSwitch(RestrictedAreaCell, Settings.RestrictedAreaString);
-			initializeSlider(HotSpotHeightSlider, Settings.HotSpotHeightString);
-			initializeSlider(HotSpotWidthSlider, Settings.HotSpotWidthString);
-			initializeSlider(HotSpotYSlider, Settings.HotSpotYString);
+            // Initialize Hotspot
+            initializeSwitch(RestrictedAreaCell, Settings.RestrictedAreaString);
+            initializeSlider(HotSpotHeightSlider, Settings.HotSpotHeightString);
+            initializeSlider(HotSpotWidthSlider, Settings.HotSpotWidthString);
+            initializeSlider(HotSpotYSlider, Settings.HotSpotYString);
 
-			// Initialize ViewFinder
-			initializeSlider(ViewFinderPortraitWidth, Settings.ViewFinderPortraitWidthString);
-			initializeSlider(ViewFinderPortraitHeight, Settings.ViewFinderPortraitHeightString);
-			initializeSlider(ViewFinderLandscapeWidth, Settings.ViewFinderLandscapeWidthString);
-			initializeSlider(ViewFinderLandscapeHeight, Settings.ViewFinderLandscapeHeightString);
+            // Initialize ViewFinder
+            initializeSlider(ViewFinderPortraitWidth, Settings.ViewFinderPortraitWidthString);
+            initializeSlider(ViewFinderPortraitHeight, Settings.ViewFinderPortraitHeightString);
+            initializeSlider(ViewFinderLandscapeWidth, Settings.ViewFinderLandscapeWidthString);
+            initializeSlider(ViewFinderLandscapeHeight, Settings.ViewFinderLandscapeHeightString);
 
             // Initialize Resolution
             initializeResolutionPicker();
-		}
+        }
 
-		// Adds a new switch (two in case the symbology has an inverse) to SymbologySection
-		void addSymbologySwitches(String settingString, int offset)
-		{
+        // Adds a new switch (two in case the symbology has an inverse) to SymbologySection
+        void addSymbologySwitches(String settingString, int offset)
+        {
             // DPM Mode switch will be created alongside with the DataMatrix one,
             // as DPM Mode availability depends on DataMatrix being enabled
             if (Settings.isDpmMode(settingString))
@@ -98,25 +98,25 @@ namespace ExtendedSample
 
             // add switch for regular symbology
             SwitchCell cell = new SwitchCell { Text = Convert.settingToDisplay[settingString] };
-			initializeSwitch(cell, settingString);
-			SymbologySection.Insert(SymbologySection.Count - offset, cell);
+            initializeSwitch(cell, settingString);
+            SymbologySection.Insert(SymbologySection.Count - offset, cell);
 
-			if (Settings.hasInvertedSymbology(settingString))
-			{
-				string invString = Settings.getInvertedSymbology(settingString);
+            if (Settings.hasInvertedSymbology(settingString))
+            {
+                string invString = Settings.getInvertedSymbology(settingString);
 
-				// add switch for inverse symbology
-				SwitchCell invCell = new SwitchCell { Text = Convert.settingToDisplay[invString] };
-				initializeSwitch(invCell, invString);
-				SymbologySection.Insert(SymbologySection.Count - offset, invCell);
+                // add switch for inverse symbology
+                SwitchCell invCell = new SwitchCell { Text = Convert.settingToDisplay[invString] };
+                initializeSwitch(invCell, invString);
+                SymbologySection.Insert(SymbologySection.Count - offset, invCell);
 
-				// Gray out the inverse symbology switch if the regular symbology is disabled
-				invCell.IsEnabled = Settings.getBoolSetting(settingString);
-				cell.OnChanged += (object sender, ToggledEventArgs e) =>
-				{
-					invCell.IsEnabled = e.Value;
-				};
-			}
+                // Gray out the inverse symbology switch if the regular symbology is disabled
+                invCell.IsEnabled = Settings.getBoolSetting(settingString);
+                cell.OnChanged += (object sender, ToggledEventArgs e) =>
+                {
+                    invCell.IsEnabled = e.Value;
+                };
+            }
 
             if (Settings.isDataMatrix(settingString))
             {
@@ -136,105 +136,105 @@ namespace ExtendedSample
             }
         }
 
-		// Bind an existing Slider to the permanent storage
-		// and the currently active settings
-		private void initializeSlider(Slider slider, string setting)
-		{
-			slider.Value = Settings.getDoubleSetting(setting);
+        // Bind an existing Slider to the permanent storage
+        // and the currently active settings
+        private void initializeSlider(Slider slider, string setting)
+        {
+            slider.Value = Settings.getDoubleSetting(setting);
 
-			slider.ValueChanged += (object sender, ValueChangedEventArgs e) =>
-				{
-					Settings.setDoubleSetting(setting, e.NewValue);
-					updateScanOverlay();
-					updateScanSettings();
-				};
-		}
+            slider.ValueChanged += (object sender, ValueChangedEventArgs e) =>
+                {
+                    Settings.setDoubleSetting(setting, e.NewValue);
+                    updateScanOverlay();
+                    updateScanSettings();
+                };
+        }
 
-		// Bind an existing Slider to the permanent storage
-		// and the currently active settings
-		private void initializeIntSlider(Slider slider, string setting)
-		{
-			slider.Value = Settings.getIntSetting(setting);
-			slider.ValueChanged += (object sender, ValueChangedEventArgs e) =>
-				{
-					int newValue = (int) Math.Round(e.NewValue);
-					slider.Value = newValue;
-					Settings.setIntSetting(setting, newValue);
-					updateScanOverlay();
-					updateScanSettings();
-				};
-		}
+        // Bind an existing Slider to the permanent storage
+        // and the currently active settings
+        private void initializeIntSlider(Slider slider, string setting)
+        {
+            slider.Value = Settings.getIntSetting(setting);
+            slider.ValueChanged += (object sender, ValueChangedEventArgs e) =>
+                {
+                    int newValue = (int)Math.Round(e.NewValue);
+                    slider.Value = newValue;
+                    Settings.setIntSetting(setting, newValue);
+                    updateScanOverlay();
+                    updateScanSettings();
+                };
+        }
 
-		// Bind an existing switch cell to the permanent storage
-		// and the currently active settings
-		private void initializeSwitch(SwitchCell cell, string setting)
-		{
-			cell.On = Settings.getBoolSetting(setting);
+        // Bind an existing switch cell to the permanent storage
+        // and the currently active settings
+        private void initializeSwitch(SwitchCell cell, string setting)
+        {
+            cell.On = Settings.getBoolSetting(setting);
 
-			cell.OnChanged += (object sender, ToggledEventArgs e) =>
-				{
-					Settings.setBoolSetting(setting, e.Value);
-					updateScanOverlay();
-					updateScanSettings();
-				};
-		}
+            cell.OnChanged += (object sender, ToggledEventArgs e) =>
+                {
+                    Settings.setBoolSetting(setting, e.Value);
+                    updateScanOverlay();
+                    updateScanSettings();
+                };
+        }
 
-		// Bind an the msi plessey checksum picker to the permanent storage
-		// and the currently active settings
-		private void initializeMsiPlesseyChecksumPicker()
-		{
-			MsiPlesseyChecksumPicker.SelectedIndex =
-			        Convert.msiPlesseyChecksumToIndex[Settings.getStringSetting(Settings.MsiPlesseyChecksumString)];
+        // Bind an the msi plessey checksum picker to the permanent storage
+        // and the currently active settings
+        private void initializeMsiPlesseyChecksumPicker()
+        {
+            MsiPlesseyChecksumPicker.SelectedIndex =
+                    Convert.msiPlesseyChecksumToIndex[Settings.getStringSetting(Settings.MsiPlesseyChecksumString)];
 
-			MsiPlesseyChecksumPicker.SelectedIndexChanged += (object sender, EventArgs e) =>
-				{
-					Settings.setStringSetting(
-						Settings.MsiPlesseyChecksumString,
-						Convert.indexToMsiPlesseyChecksum[MsiPlesseyChecksumPicker.SelectedIndex]);
-					updateScanOverlay();
-					updateScanSettings();
-				};
-		}
+            MsiPlesseyChecksumPicker.SelectedIndexChanged += (object sender, EventArgs e) =>
+                {
+                    Settings.setStringSetting(
+                        Settings.MsiPlesseyChecksumString,
+                        Convert.indexToMsiPlesseyChecksum[MsiPlesseyChecksumPicker.SelectedIndex]);
+                    updateScanOverlay();
+                    updateScanSettings();
+                };
+        }
 
-		// Bind an the camera picker to the permanent storage
-		// and the currently active settings		
-		private void initializeCameraPicker()
-		{
-			CameraButtonPicker.SelectedIndex =
+        // Bind an the camera picker to the permanent storage
+        // and the currently active settings		
+        private void initializeCameraPicker()
+        {
+            CameraButtonPicker.SelectedIndex =
                   Convert.cameraButtonToIndex[Settings.getStringSetting(Settings.CameraButtonString)];
 
-			CameraButtonPicker.SelectedIndexChanged += (object sender, EventArgs e) =>
-				{
-					Settings.setStringSetting(
-						Settings.CameraButtonString,
-				        Convert.indexToCameraButton[CameraButtonPicker.SelectedIndex]);
-					updateScanOverlay();
-					updateScanSettings();
-				};
-		}
+            CameraButtonPicker.SelectedIndexChanged += (object sender, EventArgs e) =>
+                {
+                    Settings.setStringSetting(
+                        Settings.CameraButtonString,
+                        Convert.indexToCameraButton[CameraButtonPicker.SelectedIndex]);
+                    updateScanOverlay();
+                    updateScanSettings();
+                };
+        }
 
-		// Bind an the GUIStyle picker to the permanent storage
-		// and the currently active settings
-		private void initializeGuiStylePicker()
-		{
-            GuiStylePicker.SelectedIndex = 
+        // Bind an the GUIStyle picker to the permanent storage
+        // and the currently active settings
+        private void initializeGuiStylePicker()
+        {
+            GuiStylePicker.SelectedIndex =
                 Convert.guiStyleToIndex[Settings.getStringSetting(Settings.GuiStyleString)];
 
-			GuiStylePicker.SelectedIndexChanged += (object sender, EventArgs e) =>
-				{
-					Settings.setStringSetting(
-						Settings.GuiStyleString,
-						Convert.indexToGuiStyle[GuiStylePicker.SelectedIndex]);
-					updateScanOverlay();
-					updateScanSettings();
-				};
-		}
+            GuiStylePicker.SelectedIndexChanged += (object sender, EventArgs e) =>
+                {
+                    Settings.setStringSetting(
+                        Settings.GuiStyleString,
+                        Convert.indexToGuiStyle[GuiStylePicker.SelectedIndex]);
+                    updateScanOverlay();
+                    updateScanSettings();
+                };
+        }
 
         // Bind an the Resolution picker to the permanent storage
         // and the currently active settings
         private void initializeResolutionPicker()
         {
-            ResolutionPicker.SelectedIndex = 
+            ResolutionPicker.SelectedIndex =
                 Convert.resolutionToIndex[Settings.getStringSetting(Settings.ResolutionString)];
 
             ResolutionPicker.SelectedIndexChanged += (object sender, EventArgs e) =>
@@ -246,16 +246,16 @@ namespace ExtendedSample
             };
         }
 
-		// reads the values needed for ScanSettings from the Settings class
-		// and applies them to the Picker
-		void updateScanSettings()
-		{
+        // reads the values needed for ScanSettings from the Settings class
+        // and applies them to the Picker
+        void updateScanSettings()
+        {
             bool addOnEnabled = false;
             bool isScanningAreaOverridden = false;
 
             foreach (string setting in Convert.settingToSymbologies.Keys)
-			{
-				bool enabled = Settings.getBoolSetting(setting);
+            {
+                bool enabled = Settings.getBoolSetting(setting);
 
                 // DPM Mode 
                 if (Settings.isDpmMode(setting) && enabled)
@@ -272,49 +272,51 @@ namespace ExtendedSample
                     continue;
                 }
 
-				foreach (Symbology sym in Convert.settingToSymbologies[setting])
-				{
-					_scanSettings.EnableSymbology(sym, enabled);
-					if (Settings.hasInvertedSymbology(setting))
-					{
-						_scanSettings.Symbologies[sym].ColorInvertedEnabled = Settings.getBoolSetting(
-							Settings.getInvertedSymbology(setting));
-					}
+                foreach (Symbology sym in Convert.settingToSymbologies[setting])
+                {
+                    _scanSettings.EnableSymbology(sym, enabled);
+                    if (Settings.hasInvertedSymbology(setting))
+                    {
+                        _scanSettings.Symbologies[sym].ColorInvertedEnabled = Settings.getBoolSetting(
+                            Settings.getInvertedSymbology(setting));
+                    }
 
-                    if (enabled && (sym == Symbology.TwoDigitAddOn 
-                                    || sym == Symbology.FiveDigitAddOn)) {
+                    if (enabled && (sym == Symbology.TwoDigitAddOn
+                                    || sym == Symbology.FiveDigitAddOn))
+                    {
                         addOnEnabled = true;
                     }
-				}
-			}
+                }
+            }
 
-            if (addOnEnabled) {
+            if (addOnEnabled)
+            {
                 _scanSettings.MaxNumberOfCodesPerFrame = 2;
             }
 
-			_scanSettings.Symbologies[Symbology.MsiPlessey].Checksums = 
-				Convert.msiPlesseyChecksumToScanSetting[Settings.getStringSetting(Settings.MsiPlesseyChecksumString)];
+            _scanSettings.Symbologies[Symbology.MsiPlessey].Checksums =
+                Convert.msiPlesseyChecksumToScanSetting[Settings.getStringSetting(Settings.MsiPlesseyChecksumString)];
 
-			_scanSettings.RestrictedAreaScanningEnabled = Settings.getBoolSetting(Settings.RestrictedAreaString);
-			if (_scanSettings.RestrictedAreaScanningEnabled && !isScanningAreaOverridden)
-			{
-				Double HotSpotHeight = Settings.getDoubleSetting(Settings.HotSpotHeightString);
-				Double HotSpotWidth = Settings.getDoubleSetting(Settings.HotSpotWidthString);
-				Double HotSpotY = Settings.getDoubleSetting(Settings.HotSpotYString);
+            _scanSettings.RestrictedAreaScanningEnabled = Settings.getBoolSetting(Settings.RestrictedAreaString);
+            if (_scanSettings.RestrictedAreaScanningEnabled && !isScanningAreaOverridden)
+            {
+                Double HotSpotHeight = Settings.getDoubleSetting(Settings.HotSpotHeightString);
+                Double HotSpotWidth = Settings.getDoubleSetting(Settings.HotSpotWidthString);
+                Double HotSpotY = Settings.getDoubleSetting(Settings.HotSpotYString);
 
-				Rect restricted = new Rect(0.5f - HotSpotWidth * 0.5f, HotSpotY - 0.5f * HotSpotHeight,
-										   HotSpotWidth, HotSpotHeight);
+                Rect restricted = new Rect(0.5f - HotSpotWidth * 0.5f, HotSpotY - 0.5f * HotSpotHeight,
+                                           HotSpotWidth, HotSpotHeight);
 
-				_scanSettings.ScanningHotSpot = new Scandit.BarcodePicker.Unified.Point(
-						0.5, Settings.getDoubleSetting(Settings.HotSpotYString));
-				_scanSettings.ActiveScanningAreaPortrait = restricted;
-				_scanSettings.ActiveScanningAreaLandscape = restricted;
-			}
-            _scanSettings.ResolutionPreference = 
+                _scanSettings.ScanningHotSpot = new Scandit.BarcodePicker.Unified.Point(
+                        0.5, Settings.getDoubleSetting(Settings.HotSpotYString));
+                _scanSettings.ActiveScanningAreaPortrait = restricted;
+                _scanSettings.ActiveScanningAreaLandscape = restricted;
+            }
+            _scanSettings.ResolutionPreference =
                 Convert.resolutionToScanSetting[Settings.getStringSetting(Settings.ResolutionString)];
-            
-			_picker.ApplySettingsAsync(_scanSettings);
-		}
+
+            _picker.ApplySettingsAsync(_scanSettings);
+        }
 
         // reads the values needed for ScanOverlay from the Settings class
         // and applies them to the Picker
@@ -339,6 +341,6 @@ namespace ExtendedSample
             _picker.ScanOverlay.GuiStyle =
                 Convert.guiStyleToScanSetting[Settings.getStringSetting(Settings.GuiStyleString)];
         }
-	}
+    }
 }
 
