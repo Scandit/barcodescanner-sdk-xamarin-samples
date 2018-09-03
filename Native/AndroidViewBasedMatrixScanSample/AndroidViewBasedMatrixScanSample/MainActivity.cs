5d3b7ce9a8c76a83fe48a6c3a6cddffb7c3b1179
Tomasz Jurkowski <tomasz@scandit.com>
2018-09-03 17:13:56 +0200

    Initial commit for the AndroidViewBasedMatrixScanSample.

---
+++ Native/AndroidViewBasedMatrixScanSample/AndroidViewBasedMatrixScanSample/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidViewBasedMatrixScanSample
+{
+    [Activity(Label = "AndroidViewBasedMatrixScanSample", MainLauncher = true, Icon = "@mipmap/ic_launcher")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
