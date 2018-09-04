c19eee0439b8c66c2ca5be22d4bfed7e2f493c24
Tomasz Jurkowski <tomasz@scandit.com>
2018-09-04 11:54:48 +0200

    More resources added to the sample.

--- Native/AndroidViewBasedMatrixScanSample/AndroidViewBasedMatrixScanSample/MainActivity.cs
+++ Native/AndroidViewBasedMatrixScanSample/AndroidViewBasedMatrixScanSample/MainActivity.cs
@@ -1,18 +1,38 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
+using ScanditBarcodePicker.Android;
+using Android.Support.V7.App;
+using Android.Support.V4.App;
 
 namespace AndroidViewBasedMatrixScanSample
 {
+    /*
+     * This example shows how to use the view-based Matrix Scan of the Scandit BarcodeScanner SDK, which
+     * is a high-level abstraction of the base Scandit Matrix Scan.
+     *
+     * The sample demonstrates how the view-based Matrix Scan can be used to track multiple barcodes
+     * simultaneously and how to apply multiple overlays over the detected barcodes.
+     */
     [Activity(Label = "AndroidViewBasedMatrixScanSample", MainLauncher = true, Icon = "@mipmap/ic_launcher")]
-    public class MainActivity : Activity
+    public class MainActivity : AppCompatActivity
     {
+        // Enter your Scandit SDK License key here.
+        // Your Scandit SDK License key is available via your Scandit SDK web account.
+        private const string scanditSdkAppKey = "-- ENTER YOUR SCANDIT LICENSE KEY HERE --";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
+            // Set the app key before instantiating the picker.
+            ScanditLicense.AppKey = scanditSdkAppKey;
+
             SetContentView(Resource.Layout.Main);
+
+            var fragmentManager = SupportFragmentManager;
+            fragmentManager.BeginTransaction()
+                           //.Add(Resource.Id.fragment_container, new ShelfManagementFragment())
+                           .Commit();
         }
     }
 }
