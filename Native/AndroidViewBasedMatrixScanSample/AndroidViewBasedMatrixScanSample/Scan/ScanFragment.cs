1a51ff42e999da0eeae62b0b5f49f939590f8dc1
Tomasz Jurkowski <tomasz@scandit.com>
2018-09-04 14:18:24 +0200

    ScanFragment implemented.

--- Native/AndroidViewBasedMatrixScanSample/AndroidViewBasedMatrixScanSample/Scan/ScanFragment.cs
+++ Native/AndroidViewBasedMatrixScanSample/AndroidViewBasedMatrixScanSample/Scan/ScanFragment.cs
@@ -1,4 +1,5 @@
 ﻿using ScanditBarcodePicker.Android;
+using ScanditBarcodePicker.Android.Recognition;
 
 namespace AndroidViewBasedMatrixScanSample.Scan
 {
@@ -8,7 +9,7 @@ namespace AndroidViewBasedMatrixScanSample.Scan
      */
     public abstract class ScanFragment : CameraPermissionFragment
     {
-        enum ScanState
+        protected enum ScanState
         {
             STOPPED, SCANNING
         }
@@ -17,5 +18,111 @@ namespace AndroidViewBasedMatrixScanSample.Scan
 
         protected BarcodePicker picker;
 
+        protected void InitializePicker()
+        {
+            if (picker == null)
+            {
+                picker = new BarcodePicker(Context, GetScanSettings());
+            }
+        }
+
+        public override void OnStart()
+        {
+            base.OnStart();
+            InitializePicker();
+
+            picker.ApplyScanSettings(GetScanSettings());
+            var view = View;
+            if (view != null)
+            {
+                view.Post(() => {
+                    UpdateScanUi(picker);
+                });
+            }
+
+            SetScanState(ScanState.SCANNING);
+        }
+
+        public override void OnStop()
+        {
+            base.OnStop();
+            SetScanState(ScanState.STOPPED);
+        }
+
+        public override void OnResume()
+        {
+            base.OnResume();
+            SetScanState(ScanState.SCANNING);
+        }
+
+        public override void OnPause()
+        {
+            base.OnPause();
+            SetScanState(ScanState.STOPPED);
+        }
+
+        protected ScanSettings GetScanSettings()
+        {
+            ScanSettings settings = ScanSettings.Create();
+            settings.SetSymbologyEnabled(Barcode.SymbologyEan13, true);
+            settings.SetSymbologyEnabled(Barcode.SymbologyUpce, true);
+            settings.SetSymbologyEnabled(Barcode.SymbologyUpca, true);
+            settings.SetSymbologyEnabled(Barcode.SymbologyEan8, true);
+            settings.MatrixScanEnabled = true;
+            settings.MaxNumberOfCodesPerFrame = 12;
+            settings.HighDensityModeEnabled = true;
+            settings.CodeCachingDuration = 0;
+            settings.CodeDuplicateFilter = 0;
+
+            return settings;
+        }
+
+        protected void UpdateScanUi(BarcodePicker picker)
+        {
+            InitializePicker();
+
+            picker.OverlayView.SetGuiStyle(ScanOverlay.GuiStyleNone);
+            picker.OverlayView.SetTorchEnabled(false);
+            picker.OverlayView.SetViewfinderDimension(0.9f, 0.75f, 0.95f, 0.9f);
+        }
+
+        protected void SetScanState(ScanState state)
+        {
+            SetScanState(state, null);
+        }
+
+        protected void SetScanState(ScanState state, IScanSession scanSession)
+        {
+            scanState = state;
+
+            switch (state)
+            {
+                case ScanState.STOPPED:
+                    if (scanSession != null)
+                    {
+                        scanSession.StopScanning();
+                    }
+                    else
+                    {
+                        picker.StopScanning();
+                    }
+                    break;
+                case ScanState.SCANNING:
+                    if (HasCameraPermission())
+                    {
+                        picker.StartScanning();
+                    }
+                    else
+                    {
+                        RequestCameraPermission();
+                    }
+                    break;
+            }
+        }
+
+        public override void OnCameraPermissionGranted()
+        {
+            SetScanState(scanState);
+        }
     }
 }
---
+++ Native/AndroidViewBasedMatrixScanSample/AndroidViewBasedMatrixScanSample/Scan/ShelfManagementFragment.cs
@@ -0,0 +1,11 @@
+﻿namespace AndroidViewBasedMatrixScanSample.Scan
+{
+    /**
+     * Main Fragment of the AndroidViewBasedMatrixScanSample. It prepares and manages the view of the app,
+     * as well as controls the matrix scan events/callbacks via the MatrixScanOverlayListener.
+     */
+    public class ShelfManagementFragment : ScanFragment
+    {
+
+    }
+}
