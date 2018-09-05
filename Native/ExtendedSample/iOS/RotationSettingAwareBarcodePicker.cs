638e40811b3d00e4a46a5358e5a95b04f55f72a4
Tibor Molnár <fatalaa@hotmail.com>
2018-09-05 18:03:36 +0200

    Create a BarcodePicker which does only rotates when it should (#36)
    Signed-off-by: Tibor Molnár <tibor@scandit.com>

---
+++ Native/ExtendedSample/iOS/RotationSettingAwareBarcodePicker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ScanditBarcodeScanner.iOS;
+using UIKit;
+
+namespace ExtendedSample.iOS
+{
+    public class RotationSettingAwareBarcodePicker : BarcodePicker
+    {
+        private bool _shouldAutoRotate;
+
+        public bool ShouldFollowDeviceOrientation
+        {
+            get
+            {
+                return _shouldAutoRotate;
+            }
+            set
+            {
+                _shouldAutoRotate = value;
+            }
+        }
+
+        public RotationSettingAwareBarcodePicker(ScanSettings scanSettings) : base(scanSettings) {}
+
+        public override bool ShouldAutorotate()
+        {
+            return ShouldFollowDeviceOrientation;
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            var appDelegate = (AppDelegate)UIApplication.SharedApplication.Delegate;
+            appDelegate.ShouldAutoRotate = ShouldFollowDeviceOrientation;
+        }
+
+        public override void ViewDidDisappear(bool animated)
+        {
+            base.ViewDidDisappear(animated);
+            var appDelegate = (AppDelegate)UIApplication.SharedApplication.Delegate;
+            appDelegate.ShouldAutoRotate = !ShouldFollowDeviceOrientation;
+        }
+    }
+}
