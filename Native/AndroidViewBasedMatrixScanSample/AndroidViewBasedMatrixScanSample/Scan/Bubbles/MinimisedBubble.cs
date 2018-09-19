cdc307bd1a64b3590e48b002b08e1e593bb9abae
Tomasz Jurkowski <tomasz@scandit.com>
2018-09-19 11:39:34 +0200

    Fix for devices with android API <21 applied.

--- Native/AndroidViewBasedMatrixScanSample/AndroidViewBasedMatrixScanSample/Scan/Bubbles/MinimisedBubble.cs
+++ Native/AndroidViewBasedMatrixScanSample/AndroidViewBasedMatrixScanSample/Scan/Bubbles/MinimisedBubble.cs
@@ -1,8 +1,10 @@
 ﻿using Android.Content;
 using Android.Content.Res;
 using Android.Graphics;
+using Android.OS;
 using Android.Views;
 using Android.Widget;
+using Android.Support.V4.Content;
 using AndroidViewBasedMatrixScanSample.Utility;
 
 namespace AndroidViewBasedMatrixScanSample.Scan.Bubbles
@@ -33,7 +35,16 @@ namespace AndroidViewBasedMatrixScanSample.Scan.Bubbles
         public override View GetView(Context context, LayoutInflater inflater)
         {
             var view = inflater.Inflate(Resource.Layout.BubbleMinimised, null);
-            view.FindViewById(Resource.Id.header).BackgroundTintList = ColorStateList.ValueOf(new Color(highlightColor));
+
+            // XXX A vector drawable has to be loaded with the AppCompatResources.GetDrawable method
+            // and then added programatically in order to avoid InflateException.
+            view.FindViewById(Resource.Id.triangle).Background = ContextCompat.GetDrawable(context, Resource.Drawable.bg_triangle);
+
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                view.FindViewById(Resource.Id.header).BackgroundTintList = ColorStateList.ValueOf(new Color(highlightColor));
+            }
+
             ((TextView)view.FindViewById(Resource.Id.stock)).Text = bubbleData.Stock.ToString();
             view.LayoutParameters = new FrameLayout.LayoutParams(
                 UiUtils.PxFromDp(context, WIDTH), UiUtils.PxFromDp(context, HEIGHT));
