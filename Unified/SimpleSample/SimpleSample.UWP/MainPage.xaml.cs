8c336764578233ac2867c45e433c548067d3beb2
Adrian Dydecki <adrian.dydecki@scandit.com>
2019-09-18 13:17:48 +0200

    Simple Sample for UWP platform (Windows).

---
+++ Unified/SimpleSample/SimpleSample.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SimpleSample.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            LoadApplication(new SimpleSample.App());
+        }
+    }
+}
